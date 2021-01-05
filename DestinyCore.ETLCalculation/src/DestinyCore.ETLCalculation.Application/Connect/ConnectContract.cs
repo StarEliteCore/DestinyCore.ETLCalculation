980e1a6f47acac78823ac07097efb06fdc13d007
wangzewei <1585955375@qq.com>
2021-01-05 18:45:03 +0800

    【Update】添加控制台输出日志

--- DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.Application/Connect/ConnectContract.cs
+++ DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.Application/Connect/ConnectContract.cs
@@ -1,66 +1,45 @@
 ﻿using DestinyCore.ETLCalculation.Dtos;
 using DestinyCore.ETLCalculation.Shared.Extensions;
+using Microsoft.Extensions.Logging;
 using SuperSocket.Client;
 using SuperSocket.ProtoBase;
 using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DestinyCore.ETLCalculation.Application.Connect
 {
     public class ConnectContract : IConnectContract
     {
+        private readonly IServiceProvider _serviceProvider = null;
+        private readonly ILogger _logger;
         public IEasyClient<StringPackageInfo> Client { get; set; }
         private bool reconnection = false;
+
+        public ConnectContract(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _logger = _serviceProvider.GetLogger<ILogger>();
+        }
+
         public async Task ConnectAsync()
         {
-            Console.WriteLine("Client start.");
+            _logger.LogInformation("Client start Befor");
             //接收消息事件
             Client.PackageHandler += ReceiveMessage;
             //断开重连事件
             Client.Closed += Reconnection;
-            #region 废弃代码
-            //Client.Closed += async (s, p) =>
-            //{
-            //    while (!reconnection)
-            //    {
-            //        reconnection = await Client.ConnectAsync(new IPEndPoint(IPAddress.Parse("10.1.10.172"), 4052));
-            //    }
-            //    Console.WriteLine("重新连接成功");
-            //    Client.PackageHandler += ReceiveMessage;
-            //    Client.Closed += Reconnection;
-            //    Client.StartReceive();
-            //};
-
-            //接收消息
-            //Client.PackageHandler += async (s, p) =>
-            //{
-            //    List<Task> tasks = new List<Task>();
-            //    var bogylist = p.Body.FromJson<List<PerformParameter>>();
-            //    foreach (var item in bogylist)
-            //    {
-            //        tasks.Add(
-            //            Task.Run(() =>
-            //            {
-            //                _startProcessContract.StartTaskFunc(item);
-            //            }));
-            //    }
-            //    Task.WhenAll(tasks.ToArray());
-            //    Console.WriteLine($"--------------------{ p.Body}");
-            //    await Task.CompletedTask;
-            //};
-            #endregion
             //客户端连接
             var connected = await Client.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 4052));
+            _logger.LogInformation($"Client start Befor.");
             if (!connected)
             {
-                Console.WriteLine("Failed to connect the target server.");
+                _logger.LogError($"Client start Failed to connect the target server.");
                 return;
             }
-            System.Console.WriteLine("Client connect End.");
-            await Client.SendAsync(Encoding.UTF8.GetBytes($"ReceiveMessage 毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒\r\n"));
+            _logger.LogInformation($"Client Susccess End ");
+            //await Client.SendAsync(Encoding.UTF8.GetBytes($"ReceiveMessage 毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒毒\r\n"));
             //接收
             Client.StartReceive();
             while (true)
@@ -73,6 +52,7 @@ namespace DestinyCore.ETLCalculation.Application.Connect
             await Task.CompletedTask;
             List<Task> tasks = new List<Task>();
             var bogylist = packageInfo.Body.FromJson<List<PerformParameter>>();
+            _logger.LogInformation($"Client Receive Message {packageInfo.Body}");
             //foreach (var item in bogylist)
             //{
             //    tasks.Add(
--- DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.Node/Program.cs
+++ DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.Node/Program.cs
@@ -3,6 +3,7 @@ using DestinyCore.ETLCalculation.Dtos.Decoder;
 using DestinyCore.ETLCalculation.Node.Startups;
 using DestinyCore.ETLCalculation.Shared.Modules;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SuperSocket.Client;
 using SuperSocket.ProtoBase;
 using System;
@@ -15,6 +16,12 @@ namespace DestinyCore.ETLCalculation.Node
         static async Task Main(string[] args)
         {
             IServiceCollection services = new ServiceCollection();
+            services.AddLogging(builder =>
+            {
+                builder.ClearProviders();
+                builder.AddDebug();
+                builder.AddConsole();
+            });
             services.AddApplication<SuktAppConsulModule>();
             IServiceProvider serviceProvider = services.BuildServiceProvider();
             var _connect = serviceProvider.GetService<IConnectContract>();
