efc8bf1333b2d998d742a122e19e09b02dc6bdd8
wangzewei <1585955375@qq.com>
2021-02-03 17:59:56 +0800

    【Add】添加其他查询数据代码

---
+++ DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.ETLCore/ReadBlock/IReadSourceBlock.cs
@@ -0,0 +1,10 @@
+﻿using DestinyCore.ETLCalculation.Shared;
+using System.Threading.Tasks.Dataflow;
+
+namespace DestinyCore.ETLCalculation.ETLCore.ReadBlock
+{
+    public interface IReadSourceBlock<TOutPut> : ISourceBlock<TOutPut>, IScopedDependency
+    {
+        ISourceBlock<TOutPut> GetData();
+    }
+}
---
+++ DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.ETLCore/ReadBlock/ReadDataTableBlock.cs
@@ -0,0 +1,7 @@
+﻿namespace DestinyCore.ETLCalculation.ETLCore.ReadBlock
+{
+    public class ReadDataTableBlock
+    {
+
+    }
+}
