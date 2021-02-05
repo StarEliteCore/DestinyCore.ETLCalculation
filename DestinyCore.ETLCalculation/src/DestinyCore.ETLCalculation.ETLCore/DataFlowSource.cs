51d7af38369b109eaeae2878765621a428962b3d
wangzewei <1585955375@qq.com>
2021-02-05 13:51:43 +0800

    Add准备封装基础DataFlow以此来简洁代码

---
+++ DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.ETLCore/DataFlowSource.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace DestinyCore.ETLCalculation.ETLCore
+{
+    public abstract class DataFlowSource<TOutput> : IDataFlowSource<TOutput>
+    {
+        public abstract ISourceBlock<TOutput> SourceBlock { get; }
+
+    }
+}
---
+++ DestinyCore.ETLCalculation/src/DestinyCore.ETLCalculation.ETLCore/IDataFlowSource.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace DestinyCore.ETLCalculation.ETLCore
+{
+    public interface IDataFlowSource<TOutput>
+    {
+        ISourceBlock<TOutput> SourceBlock { get; }
+    }
+}
