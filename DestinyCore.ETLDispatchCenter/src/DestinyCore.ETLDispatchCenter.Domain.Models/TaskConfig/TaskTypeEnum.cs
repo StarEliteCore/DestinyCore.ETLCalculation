534f58af58c00d5c205429d7cc19f9bef8702828
wangzewei <1585955375@qq.com>
2021-04-13 16:44:10 +0800

    【Update】修改数据库文件名称

--- DestinyCore.ETLDispatchCenter/src/DestinyCore.ETLDispatchCenter.Domain.Models/TaskConfig/TaskTypeEnum.cs
+++ DestinyCore.ETLDispatchCenter/src/DestinyCore.ETLDispatchCenter.Domain.Models/TaskConfig/TaskTypeEnum.cs
@@ -8,22 +8,17 @@ namespace DestinyCore.ETLDispatchCenter.Domain.Models.TaskConfig
         /// <summary>
         /// 数据库导入数据库任务
         /// </summary>
-        [Description("数据库导入数据库")]
-        DataBaseToDataBase = 0,
+        [Description("数据库")]
+        DataBase = 0,
         /// <summary>
         /// Http任务
         /// </summary>
-        [Description("Http")]
-        Http = 5,
+        [Description("FtpJson")]
+        FtpJson = 5,
         /// <summary>
         /// Excel导入数据库任务
         /// </summary>
-        [Description("Excel导入数据库")]
-        ExcelToDataBase = 10,
-        /// <summary>
-        /// 数据库导出Excel任务
-        /// </summary>
-        [Description("数据库导出Excel")]
-        DataBaseToExcel = 15,
+        [Description("Excel")]
+        Excel = 10,
     }
 }
